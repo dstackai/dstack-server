077bb6e3204c7a0ad4f8425270f212385812a33c
olgenn <vavilovolegik@gmail.com>
2021-02-26 13:05:42 +0300

    Fix checkbox

--- website/src/components/stack/DetailsApp/components/Views/CheckboxView/index.js
+++ website/src/components/stack/DetailsApp/components/Views/CheckboxView/index.js
@@ -1,6 +1,5 @@
 // @flow
 import React, {useEffect, useState} from 'react';
-import {useDebounce} from 'hooks';
 import CheckboxField from 'components/CheckboxField';
 
 import type {TView} from '../types';
@@ -17,18 +16,16 @@ type Props = {
     debounce?: number,
 }
 
-const CheckboxView = ({className, view, disabled, debounce = 300, onChange: onChangeProp}: Props) => {
+const CheckboxView = ({className, view, disabled, onChange: onChangeProp}: Props) => {
     const [value, setValue] = useState(Boolean(view.selected));
 
     useEffect(() => setValue(Boolean(view.selected)), [view]);
-
-    const onChangePropDebounce = useDebounce(onChangeProp, debounce, [debounce, onChangeProp]);
 
     const onChange = (event: Event<HTMLInputElement>) => {
         setValue(!!event.target?.checked);
 
         if (onChangeProp) {
-            onChangePropDebounce({
+            onChangeProp({
                 ...view,
                 selected: !!event.target?.checked,
             });
